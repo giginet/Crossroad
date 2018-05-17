e9b3bb84e0c62f26b9b88ca2d55c7ba1a143ddfe
giginet <giginet.net@gmail.com>
2018-05-17 18:01:02 +0900

    Initial Commit :hatched_chick:

---
+++ Sources/Junction/Junction.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Junction.
+FOUNDATION_EXPORT double JunctionVersionNumber;
+
+//! Project version string for Junction.
+FOUNDATION_EXPORT const unsigned char JunctionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Junction/PublicHeader.h>
+
+
