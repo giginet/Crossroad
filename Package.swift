6464a832b1151039b70585662dc716697f6c2e39
giginet <giginet.net@gmail.com>
2018-05-20 05:00:18 +0900

    Modify Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,5 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
@@ -5,16 +7,20 @@ let package = Package(
     products: [
         .library(
             name: "Crossroad",
-            targets: ["Crossroad"]),
+            targets: ["Crossroad"])
     ],
     dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Crossroad",
             dependencies: []),
         .testTarget(
             name: "CrossroadTests",
-            dependencies: ["Crossroad"]),
+            dependencies: ["Crossroad"])
     ]
 )
