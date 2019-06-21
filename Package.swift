99dc2eced631803ca985fd96370b86ae9ebeef8f
giginet <giginet.net@gmail.com>
2019-06-21 18:08:49 +0900

    Support SwiftPM for Xcode11

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Crossroad",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "Crossroad",
+            targets: ["Crossroad"]),
+    ],
+    targets: [
+        .target(
+            name: "Crossroad",
+            dependencies: []),
+        .testTarget(
+            name: "CrossroadTests",
+            dependencies: ["Crossroad"]),
+    ]
+)
