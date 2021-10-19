e04d5017271ee5f26ea016315d715defdc3199d8
giginet <giginet.net@gmail.com>
2021-10-19 15:12:38 +0900

    Test grouped routes

--- Tests/CrossroadTests/Router_AcceptanceTests.swift
+++ Tests/CrossroadTests/Router_AcceptanceTests.swift
@@ -9,11 +9,32 @@ final class Router_AcceptanceTests: XCTestCase {
     func testAcceptOnly() throws {
         let router = try SimpleRouter(accepts: [customURLScheme, universalLink]) { route in
             route("/pokemons/:id", accepts: .only(for: universalLink)) { _ in
-                return true
+                true
             }
         }
 
         XCTAssertFalse(router.responds(to: URL(string: "pokedex://pokemons/:id")!))
         XCTAssertTrue(router.responds(to: URL(string: "https://my-awesome-pokedex.com/pokemons/:id")!))
+    }
+
+    func testAcceptOnlyWithGroup() throws {
+        let router = try SimpleRouter(accepts: [customURLScheme, universalLink]) { route in
+            route.group(accepts: [universalLink]) { route in
+                route("/pokemons/:id") { _ in
+                    true
+                }
+            }
+
+            route.group(accepts: [customURLScheme]) { route in
+                route("/moves/:id") { _ in
+                    true
+                }
+            }
+        }
+
+        XCTAssertFalse(router.responds(to: URL(string: "pokedex://pokemons/:id")!))
+        XCTAssertTrue(router.responds(to: URL(string: "https://my-awesome-pokedex.com/pokemons/:id")!))
+        XCTAssertTrue(router.responds(to: URL(string: "pokedex://moves/:id")!))
+        XCTAssertFalse(router.responds(to: URL(string: "https://my-awesome-pokedex.com/moves/:id")!))
     }
 }
