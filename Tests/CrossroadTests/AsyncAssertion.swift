390ca84e1403bef88dabbfc58414650aa71f9a33
Kohki Miki <giginet.net@gmail.com>
2021-12-15 21:50:32 +0900

    Update Tests/CrossroadTests/AsyncAssertion.swift
    Co-authored-by: uhooi <the_uhooi@yahoo.co.jp>

--- Tests/CrossroadTests/AsyncAssertion.swift
+++ Tests/CrossroadTests/AsyncAssertion.swift
@@ -3,9 +3,7 @@ import XCTest
 
 func assertTrueAsynchronously(_ closure: @autoclosure () async -> Bool, message: String = "", file: StaticString = #file, line: UInt = #line) async {
     let result = await closure()
-    if !result {
-        XCTFail(message, file: file, line: line)
-    }
+    XCTAssertTrue(result, message, file: file, line: line)
 }
 
 func assertFalseAsynchronously(_ closure: @autoclosure () async -> Bool, message: String = "", file: StaticString = #file, line: UInt = #line) async {
